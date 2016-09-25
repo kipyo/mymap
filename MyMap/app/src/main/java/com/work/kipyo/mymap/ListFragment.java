cc561079207704a58d3825a0d7094e38cf399191
kipyo <roadkp@naver.com>
2016-09-25 15:01:07 +0900

    CompileErrorFix
    삭제된 class의 import부분 삭제

--- MyMap/app/src/main/java/com/work/kipyo/mymap/ListFragment.java
+++ MyMap/app/src/main/java/com/work/kipyo/mymap/ListFragment.java
@@ -13,8 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.work.kipyo.mymap.test.TestCursor;
-
 /**
  * Created by kipyo on 2016-09-23.
  */
